e0ea3f457defa6d67b29ea4527d1fb1bf75b8c0a
ipmcgrat <49279110+ipmcgrat@users.noreply.github.com>
2019-09-15 22:39:56 -0400

    Add files via upload
    fixed errors in console

--- public/js/randominfo.js
+++ public/js/randominfo.js
@@ -1,11 +1,61 @@
 var waitingApp = new Vue({
   el: '#randomuserinfo',
   data: {
-    person: [
-        {
+    person:
+    {
+    "gender": "",
+          "name": {
+            "title": "",
+            "first": "",
+            "last": ""
+          },
+          "location": {
+            "street": "",
+            "city": "",
+            "state": "",
+            "postcode": "",
+            "coordinates": {
+              "latitude": "",
+              "longitude": ""
+            },
+            "timezone": {
+              "offset": "",
+              "description": ""
+            }
+          },
+          "email": "",
+          "login": {
+            "uuid": "",
+            "username": "",
+            "password": "",
+            "salt": "",
+            "md5": "",
+            "sha1": "",
+            "sha256": ""
+          },
+          "dob": {
+            "date": "",
+            "age": null
+          },
+          "registered": {
+            "date": "",
+            "age": null
+          },
+          "phone": "",
+          "cell": "",
+          "id": {
+            "name": "",
+            "value": ""
+          },
+          "picture": {
+            "large": "",
+            "medium": "",
+            "thumbnail": ""
+          },
+          "nat": ""
         }
-    ]
-  },
+      },
+
   methods: {
     fetchPatients() {
       fetch('https://randomuser.me/api/')
