6684ffa548c0c5881ba95edf7e88e5acc4c23b42
ipmcgrat <49279110+ipmcgrat@users.noreply.github.com>
2019-09-22 17:26:47 -0400

    fixed folder structure to include app

---
+++ app/public/js/randominfo.js
@@ -0,0 +1,75 @@
+var waitingApp = new Vue({
+  el: '#randomuserinfo',
+  data: {
+    person:
+    {
+    "gender": "",
+          "name": {
+            "title": "",
+            "first": "",
+            "last": ""
+          },
+          "location": {
+            "street": "",
+            "city": "",
+            "state": "",
+            "postcode": "",
+            "coordinates": {
+              "latitude": "",
+              "longitude": ""
+            },
+            "timezone": {
+              "offset": "",
+              "description": ""
+            }
+          },
+          "email": "",
+          "login": {
+            "uuid": "",
+            "username": "",
+            "password": "",
+            "salt": "",
+            "md5": "",
+            "sha1": "",
+            "sha256": ""
+          },
+          "dob": {
+            "date": "",
+            "age": null
+          },
+          "registered": {
+            "date": "",
+            "age": null
+          },
+          "phone": "",
+          "cell": "",
+          "id": {
+            "name": "",
+            "value": ""
+          },
+          "picture": {
+            "large": "",
+            "medium": "",
+            "thumbnail": ""
+          },
+          "nat": ""
+        }
+      },
+
+  methods: {
+    fetchPatients() {
+      fetch('https://randomuser.me/api/')
+      .then(response => response.json())
+      .then(json => waitingApp.person = json.results[0]);
+
+      // Means the same at this
+      // fetch('https://randomuser.me/api/')
+      // .then(function(response) {return response.json()})
+      // .then(function(json) {waitingApp.people = json});
+
+    }
+  },
+  created() {
+    this.fetchPatients();
+  }
+});
